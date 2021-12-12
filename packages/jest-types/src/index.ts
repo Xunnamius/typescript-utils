b1c37f0332c5408202d7642769f3a1aeedfa7192
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2021-12-11 18:25:02 -0800

    fix(packages): use proper monorepo imports and dependencies

--- packages/jest-types/src/index.ts
+++ packages/jest-types/src/index.ts
@@ -1,2 +1,12 @@
-export * from './utils';
-export * from 'pkgverse/types/src';
+import type { AnyFunction } from '@xunnamius/types';
+// ? Pull in jest type definitions without importing jest or causing errors
+import type {} from 'jest';
+
+/**
+ * Returns `fn` (via `jest.fn()`) wrapped with Jest mock type definitions.
+ */
+export function asMockedFunction<T extends AnyFunction = never>(): jest.MockedFunction<T>;
+export function asMockedFunction<T extends AnyFunction>(fn: T): jest.MockedFunction<T>;
+export function asMockedFunction<T extends AnyFunction>(fn?: T): jest.MockedFunction<T> {
+  return (fn || jest.fn()) as unknown as jest.MockedFunction<T>;
+}
