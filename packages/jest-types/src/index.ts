11234c0202b421ce874fbe9b702c1930d1322981
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2021-12-26 09:29:28 -0800

    feat(packages/jest-types): add asMockedClass to complement asMockedFunction

--- packages/jest-types/src/index.ts
+++ packages/jest-types/src/index.ts
@@ -1,6 +1,5 @@
 import type { AnyFunction } from '@xunnamius/types';
 // ? Pull in jest type definitions without importing jest or causing errors
-import type {} from 'jest';
 
 /**
  * Returns `fn` (via `jest.fn()`) wrapped with Jest mock type definitions.
@@ -9,4 +8,20 @@ export function asMockedFunction<T extends AnyFunction = never>(): jest.MockedFu
 export function asMockedFunction<T extends AnyFunction>(fn: T): jest.MockedFunction<T>;
 export function asMockedFunction<T extends AnyFunction>(fn?: T): jest.MockedFunction<T> {
   return (fn || jest.fn()) as unknown as jest.MockedFunction<T>;
+}
+
+/**
+ * Returns `classConstructor` (via `jest.fn()`) wrapped with Jest mock type
+ * definitions.
+ */
+export function asMockedClass<
+  T extends jest.Constructable = never
+>(): jest.MockedClass<T>;
+export function asMockedClass<T extends jest.Constructable>(
+  constructor: T
+): jest.MockedClass<T>;
+export function asMockedClass<T extends jest.Constructable>(
+  constructor?: T
+): jest.MockedClass<T> {
+  return (constructor || jest.fn()) as unknown as jest.MockedClass<T>;
 }
